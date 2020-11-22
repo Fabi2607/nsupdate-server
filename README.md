39248419e1b38a7d80bcfd29462fcb0860292c6e
Fabian Kantereit <fabian.kantereit@tsaro.io>
2020-11-22 16:05:05 +0100

    main: Add configuration via environment variables
    Signed-off-by: Fabian Kantereit <fabian.kantereit@tsaro.io>

---
+++ README.md
@@ -0,0 +1,18 @@
+## RNU: Simple NS update server written in Rust
+
+## Setup
+
+The server can be configured using the following environment variables:
+
+- `RNU_DEBUG` enables debug mode and just outputs performed operation to stdout
+- `RNU_AUTH_FILE` (default "keys") File that contains pairs of API key and domain
+- `RNU_LISTEN` (default "127.0.0.1:3000") Address and port to bind to
+
+## Usage
+
+Send a GET request to:
+```
+http://<host>:<port>/update?key=<key>&domain=<domain>&ip=<ipv4>&ipv6=<ipv6>
+```
+
+You need to specify at least one of the IP parameters. 
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
 use std::collections::HashMap;
 use std::convert::Infallible;
+use std::env;
 use std::fs;
 use std::fs::File;
-use std::net::SocketAddr;
+use std::net::{SocketAddr, IpAddr};
 use std::process::{Command, Stdio};
 
 use hyper::{Body, Method, Request, Response, Server, StatusCode, Uri};
 use hyper::service::{make_service_fn, service_fn};
+use std::time::SystemTime;
 
 /// Update DNS record
 fn nsupdate(domain: &str, ip: &str, record: &str) {
-    fs::write("entry.nsupdate", format!(
+    let command = match env::var_os("RNU_DEBUG") {
+        Some(_) => "cat",
+        None => "nsupdate",
+    };
+
+    let mut path = env::temp_dir();
+    path.push(format!("{}.txt", SystemTime::now().elapsed().unwrap().as_nanos()));
+
+    let path = path.as_os_str();
+
+    fs::write(path, format!(
         "update del {domain} {record}\nupdate add {domain} 60 {record} {ip}\nsend\n", domain = domain, ip = ip, record = record)).expect("Could not write file");
 
-    Command::new("cat")
-        .stdin(Stdio::from(File::open("entry.nsupdate").expect("Could not find file")))
+    let status = Command::new(command)
+        .stdin(Stdio::from(File::open(path).expect("Could not find file")))
         .stdout(Stdio::inherit())
-        .spawn()
+        .status()
         .expect("Failed to execute command");
+
+    if !status.success() {
+        println!("Failed to update record");
+    }
+
+    fs::remove_file(path).expect("Failed to remove file")
 }
 
 /// Validate the API Key
 fn validate_api_key(_key: &str, _domain: &str) -> bool {
+    let path = env::var("RNU_AUTH_FILE").unwrap_or("keys".to_string());
+
     let contents =
-        fs::read_to_string("./keys")
+        fs::read_to_string(path)
             .unwrap_or("test test".to_string());
     let api_keys: Vec<&str> = contents.split("\n").collect();
 
@@ -113,7 +133,15 @@ async fn shutdown_signal() {
 
 #[tokio::main()]
 async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let ip_address: IpAddr = std::env::var("RNU_HOST")
+        .unwrap_or("127.0.0.1".to_string()).parse().expect("Failed to parse IP addr");
+
+
+    let port: u16 = std::env::var("RNU_PORT")
+        .unwrap_or("3000".to_string()).parse().expect("Failed to parse port");
+
+
+    let addr = SocketAddr::new(ip_address, port);
 
     let make_svc =
         make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
