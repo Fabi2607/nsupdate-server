d5623efc2c9eaa5cbb8eba70ea10d2b53d857f8f
Fabian Kantereit <fabian.kantereit@tsaro.io>
2020-11-23 21:52:12 +0100

    main: Reformat and add unit tests
    Signed-off-by: Fabian Kantereit <fabian.kantereit@tsaro.io>

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::convert::Infallible;
 use std::env;
 use std::fs;
 use std::fs::File;
-use std::net::{SocketAddr, IpAddr};
+use std::net::{IpAddr, SocketAddr};
 use std::process::{Command, Stdio};
 
-use hyper::{Body, Method, Request, Response, Server, StatusCode, Uri};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode, Uri};
 use std::time::SystemTime;
 
 /// Update DNS record
@@ -18,12 +18,23 @@ fn nsupdate(domain: &str, ip: &str, record: &str) {
     };
 
     let mut path = env::temp_dir();
-    path.push(format!("{}.txt", SystemTime::now().elapsed().unwrap().as_nanos()));
+    path.push(format!(
+        "{}.txt",
+        SystemTime::now().elapsed().unwrap().as_nanos()
+    ));
 
     let path = path.as_os_str();
 
-    fs::write(path, format!(
-        "update del {domain} {record}\nupdate add {domain} 60 {record} {ip}\nsend\n", domain = domain, ip = ip, record = record)).expect("Could not write file");
+    fs::write(
+        path,
+        format!(
+            "update del {domain} {record}\nupdate add {domain} 60 {record} {ip}\nsend\n",
+            domain = domain,
+            ip = ip,
+            record = record
+        ),
+    )
+    .expect("Could not write file");
 
     let status = Command::new(command)
         .stdin(Stdio::from(File::open(path).expect("Could not find file")))
@@ -39,17 +50,20 @@ fn nsupdate(domain: &str, ip: &str, record: &str) {
 }
 
 /// Validate the API Key
-fn validate_api_key(_key: &str, _domain: &str) -> bool {
+fn validate_api_key(key: &str, domain: &str) -> bool {
     let path = env::var("RNU_AUTH_FILE").unwrap_or("keys".to_string());
 
-    let contents =
-        fs::read_to_string(path)
-            .unwrap_or("test test".to_string());
-    let api_keys: Vec<&str> = contents.split("\n").collect();
+    let contents = fs::read_to_string(path).unwrap_or("".to_string());
+
+    internal_validate_api_keys(&*contents, key, domain)
+}
+
+fn internal_validate_api_keys(file_content: &str, key: &str, domain: &str) -> bool {
+    let api_keys: Vec<&str> = file_content.split("\n").collect();
 
     api_keys.iter().any(|line| {
         let parts: Vec<&str> = line.split_ascii_whitespace().collect();
-        parts == [_key, _domain]
+        parts == [key, domain]
     })
 }
 
@@ -66,18 +80,22 @@ fn handle_update(uri: &Uri) -> Result<Response<Body>, Infallible> {
 
     let domain = match params.get("domain") {
         Some(domain) => domain,
-        None => return Ok(Response::builder()
-            .status(StatusCode::BAD_REQUEST)
-            .body(Body::from("Domain must be specified"))
-            .unwrap_or(Response::default())),
+        None => {
+            return Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from("Domain must be specified"))
+                .unwrap_or(Response::default()))
+        }
     };
 
     let key = match params.get("key") {
         Some(key) => key,
-        None => return Ok(Response::builder()
-            .status(StatusCode::BAD_REQUEST)
-            .body(Body::from("No API Key specified"))
-            .unwrap_or(Response::default())),
+        None => {
+            return Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from("No API Key specified"))
+                .unwrap_or(Response::default()))
+        }
     };
 
     if !validate_api_key(key, domain) {
@@ -115,12 +133,10 @@ fn handle_update(uri: &Uri) -> Result<Response<Body>, Infallible> {
 async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/update") => handle_update(req.uri()),
-        _ => {
-            Ok(Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(Body::from("Page not found"))
-                .unwrap_or(Response::default()))
-        }
+        _ => Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from("Page not found"))
+            .unwrap_or(Response::default())),
     }
 }
 
@@ -134,12 +150,14 @@ async fn shutdown_signal() {
 #[tokio::main()]
 async fn main() {
     let ip_address: IpAddr = std::env::var("RNU_HOST")
-        .unwrap_or("127.0.0.1".to_string()).parse().expect("Failed to parse IP addr");
-
+        .unwrap_or("127.0.0.1".into())
+        .parse()
+        .expect("Failed to parse IP addr");
 
     let port: u16 = std::env::var("RNU_PORT")
-        .unwrap_or("3000".to_string()).parse().expect("Failed to parse port");
-
+        .unwrap_or("3000".into())
+        .parse()
+        .expect("Failed to parse port");
 
     let addr = SocketAddr::new(ip_address, port);
 
@@ -152,5 +170,42 @@ async fn main() {
 
     if let Err(e) = graceful.await {
         eprintln!("Server error: {}", e);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_key_file() {
+        assert_eq!(internal_validate_api_keys("", "test", "test"), false);
+    }
+
+    #[test]
+    fn test_key_file_missing_domain() {
+        assert_eq!(internal_validate_api_keys("test", "test", "test"), false);
+    }
+
+    #[test]
+    fn test_key_file_excess_content() {
+        assert_eq!(
+            internal_validate_api_keys("test test 3", "test", "test"),
+            false
+        );
+    }
+
+    #[test]
+    fn test_key_file_single_line() {
+        assert!(internal_validate_api_keys("test test", "test", "test"));
+    }
+
+    #[test]
+    fn test_key_file_multiline() {
+        assert!(internal_validate_api_keys(
+            "a a\ntest test\n5 4",
+            "test",
+            "test"
+        ));
     }
 }
